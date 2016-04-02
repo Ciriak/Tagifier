07d9795010d2a1229ee682e29aa2ae48ce161a1b
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-04-02 20:25:05 +0200

    Updating style

--- public/js/controllers/file.js
+++ public/js/controllers/file.js
@@ -4,8 +4,9 @@ app.controller('fileCtrl', function($scope,$state,$http,$stateParams)
 	$scope.userPattern;
 	$scope.pattern;
 	$scope.canDownload = false;
+	$scope.processing = false;
 	$scope.file = {};
-	$scope.buttonLabel = "Download";
+	$scope.fileHeadingLabel = {};
 
 	$scope.exportFile = {};
 	$http({
@@ -22,6 +23,8 @@ app.controller('fileCtrl', function($scope,$state,$http,$stateParams)
 		$scope.file = data;
 		$scope.exportFile.image = getBestThumbnail(data.snippet.thumbnails);
 		$scope.exportFile.id = $stateParams.fileId;
+		
+
 		var pt = data.snippet.localized.title.split(" - ");
 		$scope.userPattern = "%artist% - %title%";
 		// if xx - xx format
@@ -59,23 +62,34 @@ app.controller('fileCtrl', function($scope,$state,$http,$stateParams)
 				$scope.exportFile[$scope.vars[i]] = extrData[i+1];
 			}
 		};
+
+		$scope.fileHeadingLabel.h1 = $scope.exportFile.title;
+		$scope.fileHeadingLabel.h2 = $scope.exportFile.artist;
+
 		$scope.canDownload = true;
+		$scope.processing = false;
 	}
 
 	$scope.requestFile = function(){
+		$scope.processing = true;
 		$scope.socket.emit("fileRequest",{file:$scope.exportFile});
+		$scope.fileHeadingLabel.h2 = "In Waiting list..";
 	}
 
 	$scope.socket.on("yd_event",function(ev){
 		console.log(ev);
 		if(ev["event"] == "progress"){
-			$scope.buttonLabel = ev.data.progress.percentage;
+			$scope.processing = true;
+			$scope.fileHeadingLabel.h2 = "Processing... ("+Math.round(ev.data.progress.percentage)+"%)";
 		}
 		if(ev["event"] == "error"){
 			$scope.buttonLabel = "Download";
+			$scope.processing = false;
 		}
 		if(ev["event"] == "finished"){
 			console.log("File Ready");
+			$scope.processing = false;
+			$scope.fileHeadingLabel.h2 = "Your file is ready !";
 		}
 
 		$scope.$apply();
