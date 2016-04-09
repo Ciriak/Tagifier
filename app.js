431026e9e1797f41999f6b5273c07b77465de565
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-04-09 13:04:12 +0200

    Added an auto updater function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var request = require('request');
 var nodeID3 = require('node-id3');
 var random = require('random-gen');
+var AutoUpdater = require('auto-updater');
 var bodyParser = require('body-parser')
 var YoutubeMp3Downloader = require('youtube-mp3-downloader');
 var fid = require('fast-image-downloader');
@@ -22,22 +23,21 @@ var server = app.listen(port,function(){
 
 // dir cleaner function
 var rmDir = function(dirPath, removeSelf) {
-      if (removeSelf === undefined)
-        removeSelf = true;
-      try { var files = fs.readdirSync(dirPath); }
-      catch(e) { return; }
-      if (files.length > 0)
-        for (var i = 0; i < files.length; i++) {
-          var filePath = dirPath + '/' + files[i];
-          if (fs.statSync(filePath).isFile())
-            fs.unlinkSync(filePath);
-          else
-            rmDir(filePath);
-        }
-      if (removeSelf)
-        fs.rmdirSync(dirPath);
-    };
-
+  if (removeSelf === undefined)
+    removeSelf = true;
+  try { var files = fs.readdirSync(dirPath); }
+  catch(e) { return; }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath);
+      else
+        rmDir(filePath);
+    }
+  if (removeSelf)
+    fs.rmdirSync(dirPath);
+};
 
 rmDir('./public/img/temps',false);
 rmDir('./exports',false);
@@ -143,7 +143,6 @@ app.get('/api/:fileId(*{1,11})', function(req,res){
 });
 
 io.on('connection', function (socket){
-  console.log("User connected");
 
   socket.on('fileRequest', function (data) {
     var session = random.alphaNum(16);
@@ -202,5 +201,62 @@ io.on('connection', function (socket){
     });
   });
 });
+
+app.get('/update',function(res,ret){
+  //auto updater
+  var autoupdater = new AutoUpdater({
+   pathToJson: '',
+   autoupdate: true,
+   checkgit: true,
+   jsonhost: 'raw.githubusercontent.com',
+   contenthost: 'codeload.github.com',
+   progressDebounce: 0,
+   devmode: false
+  });
+
+  autoupdater.on('git-clone', function() {
+      console.log("You have a clone of the repository. Use 'git pull' to be up-to-date");
+    });
+    autoupdater.on('check.up-to-date', function(v) {
+      console.info("You have the latest version: " + v);
+    });
+    autoupdater.on('check.out-dated', function(v_old, v) {
+      console.warn("Your version is outdated. " + v_old + " of " + v);
+      autoupdater.fire('download-update'); // If autoupdate: false, you'll have to do this manually. 
+      // Maybe ask if the'd like to download the update. 
+    });
+    autoupdater.on('update.downloaded', function() {
+      console.log("Update downloaded and ready for install");
+      autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually. 
+    });
+    autoupdater.on('update.not-installed', function() {
+      console.log("The Update was already in your folder! It's read for install");
+      autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually. 
+    });
+    autoupdater.on('update.extracted', function() {
+      console.log("Update extracted successfully!");
+      console.warn("RESTART THE APP!");
+    });
+    autoupdater.on('download.start', function(name) {
+      console.log("Starting downloading: " + name);
+    });
+    autoupdater.on('download.progress', function(name, perc) {
+      process.stdout.write("Downloading " + perc + "% \033[0G");
+    });
+    autoupdater.on('download.end', function(name) {
+      console.log("Downloaded " + name);
+    });
+    autoupdater.on('download.error', function(err) {
+      console.error("Error when downloading: " + err);
+    });
+    autoupdater.on('end', function() {
+      console.log("The app is ready to function");
+    });
+    autoupdater.on('error', function(name, e) {
+      console.error(name, e);
+    });
+
+    autoupdater.fire('check');
+}); 
 
 app.use('/', express.static(__dirname + '/public/'));
