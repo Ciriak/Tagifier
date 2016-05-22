e4784ea0493b8376eb6cec5c4df126e2703570b3
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-05-22 12:19:27 +0200

    Updated deps

--- app.js
+++ app.js
@@ -1,8 +1,61 @@
-var http = require('http');
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/public/index.html`)
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+
 var fs = require('fs-sync');
 var ofs = require('fs');  // old fs
 var port = 80;
-var express = require('express');
 var request = require('request');
 var id3 = require('node-id3');
 var random = require('random-gen');
@@ -26,12 +79,6 @@ var fidOpt = {
   TIMEOUT : 2000, // timeout in ms
   ALLOWED_TYPES : ['jpg', 'png'] // allowed image types
 };
-
-var app = express();
-var server = app.listen(port,function(){
-  console.log("Tagifier is listening on port "+port);
-});
-server.timeout = 180000;  //3min
 
 // clean function remove all temp thumbnails and mp3
 
@@ -64,16 +111,16 @@ rmDir('./public/img/temps',false);
 rmDir('./exports',false);
 console.log("Temp files cleaned");
 
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(app);
 
 var config = {};
 
 // retreive config file
 config = fs.readJSON("config.json");
 
-app.use(compression());
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//app.use(compression());
+//app.use( bodyParser.json() );       // to support JSON-encoded bodies
+/*app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
@@ -133,7 +180,7 @@ app.get('/api/infos/:fileId(*)', function(req,res){
     }
     res.send(infos);
   });
-});
+});*/
 
 io.on('connection', function (socket){
 
@@ -451,4 +498,4 @@ function getToStr(get){
   return ret;
 }
 
-app.use('/', express.static(__dirname + '/public/'));
+//app.use('/', express.static(__dirname + '/public/'));
