7fd322868a623df15708488c1b53e3fd76ef8bb1
Cyriaque Delaunay <cyriaquedelaunay@gmail.com>
2016-06-09 12:00:22 +0200

    Added package-script as dependencies
    Give the possibility to globally install electron, gulp and bower

---
+++ postinstall.js
@@ -0,0 +1,22 @@
+require('package-script').spawn([
+      {
+          command: "npm",
+          args: ["install", "-g", "gulp"]
+      },
+      {
+          command: "npm",
+          args: ["install", "-g", "bower"]
+      },
+      {
+          command: "npm",
+          args: ["install", "-g", "electron"]
+      },
+      {
+          command: "npm",
+          args: ["install", "-g", "electron"]
+      },
+      {
+          command: "gulp",
+          args: ["prepare-dev-env"]
+      }
+  ]);
