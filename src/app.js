fe778fec83adcd2e37b28e43a72f902ed0931d6c
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-18 13:38:06 +0200

    Added auto updater module

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@
 
 const electron = require('electron');
 const app = electron.app;
+console.log("Tagifier V."+app.getVersion());
 const Menu = electron.Menu;
 const BrowserWindow = electron.BrowserWindow;
 const ipc = electron.ipcMain;
@@ -38,6 +39,36 @@ app.on('activate', function () {
     createWindow();
   }
 });
+
+//
+//  Auto updater
+//
+const GhReleases = require('electron-gh-releases')
+
+let options = {
+  repo: 'Cyriaqu3/tagifier',
+  currentVersion: app.getVersion()
+}
+
+const updater = new GhReleases(options)
+
+// Check for updates
+// `status` returns true if there is a new update available
+updater.check((err, status) => {
+  if (!err && status) {
+    // Download the update
+    updater.download()
+  }
+})
+
+// When an update has been downloaded
+updater.on('update-downloaded', (info) => {
+  // Restart the app and install the update
+  updater.install()
+})
+
+// Access electrons autoUpdater
+updater.autoUpdater
 
 var fs = require('fs-sync');
 var ofs = require('fs');  // old fs
